17417dc068295c649ff3ca1ea9b20282110b5fb7
Yoshi Nagi <nagiyoshi13@gmail.com>
2024-01-30 10:21:29 +0900

    add star rating file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import App from './App.jsx';
+// import './index.css';
+import StarRating from './StarRating.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      message={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+    />
+    <StarRating maxRating={5} color="red" size={24} defaultRating={3} />
+  </React.StrictMode>
+);
