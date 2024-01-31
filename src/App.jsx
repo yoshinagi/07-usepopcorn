5a39937486ff56fa092a475c393e55a860cedaaa
Yoshi Nagi <nagiyoshi13@gmail.com>
2024-01-31 19:00:31 +0900

    add title change

--- src/App.jsx
+++ src/App.jsx
@@ -299,6 +299,18 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     [selectedId]
   );
 
+  useEffect(
+    function () {
+      if (!title) return;
+      document.title = `Movie | ${title}`;
+
+      return function () {
+        document.title = 'usePopcorn';
+      };
+    },
+    [title]
+  );
+
   return (
     <div className="details">
       {isLoading ? (
